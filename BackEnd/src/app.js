7596fe399d14e9e7c5a6fad79dcb7951baafeeff
Hello <Thang@gmail.com>
2023-06-01 22:05:21 +0700

    Complete Connect Mongodb

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import route from "../Routes/product";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 
+const { API } = process.env;
+
 app.use("/api", route);
+mongoose.connect(API);
 
 export const viteNodeApp = app;
