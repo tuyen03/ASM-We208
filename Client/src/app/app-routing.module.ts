4d9004b018588df0372e1b1b0831f457eb730d2a
tuyennn003 <nangtuyen605@gmail.com>
2023-06-02 16:17:18 +0700

    config layout homepage

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
 
 
 const routes: Routes = [
-
+    {
+        path: "", component: HomeLayoutComponent, children: [
+            { path: "", redirectTo: "", pathMatch: "full" },
+            { path: "", component: HomePageComponent }
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLayoutComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
