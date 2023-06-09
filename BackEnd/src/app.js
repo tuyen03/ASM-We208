dec8e5b11910f87ebf945deeb3c7d4ad0c8e1815
Hello <Thang@gmail.com>
2023-06-09 09:55:30 +0700

    Update main

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -2,18 +2,16 @@ import express from "express";
 import route from "../Routes/product";
 import routeUser from "../Routes/User";
 import mongoose from "mongoose";
-import cors from 'cors'
-import Category from "../Routes/Category";
 import cors from "cors";
+import Category from "../Routes/Category";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 const { API } = process.env;
 
-app.use(cors())
+app.use(cors());
 app.use("/api", route);
 app.use("/api", routeUser);
 app.use("/api", Category);
